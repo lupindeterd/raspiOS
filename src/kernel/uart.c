38a6cacc66d71769703ab9474e2836d50bb15a13
Lupin Deterd <lupindeterd@gmail.com>
2019-12-18 18:05:01 +1300

    organise directory structure

--- src/kernel/uart.c
+++ src/kernel/uart.c
@@ -1,6 +1,4 @@
-#include <stddef.h>
-#include <stdint.h>
-
+include <uart.h>
 
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
@@ -17,36 +15,6 @@ static inline void delay(int32_t count)
    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
            : "=r"(count): [count]"0"(count) : "cc");
 }
-
-enum
-{
-    GPIO_BASE = 0x3F200000,
-
-    GPPUD = (GPIO_BASE + 0x94),
-    GPPUDCLK0 = (GPIO_BASE + 0x98),
-  
-    UART0_BASE = 0x3F201000,
-
-
-    UART0_DR     = (UART0_BASE + 0x00),
-    UART0_RSRECR = (UART0_BASE + 0x04),
-    UART0_FR     = (UART0_BASE + 0x18),
-    UART0_ILPR   = (UART0_BASE + 0x20),
-    UART0_IBRD   = (UART0_BASE + 0x24),
-    UART0_FBRD   = (UART0_BASE + 0x28),
-    UART0_LCRH   = (UART0_BASE + 0x2C),
-    UART0_CR     = (UART0_BASE + 0x30),
-    UART0_IFLS   = (UART0_BASE + 0x34),
-    UART0_IMSC   = (UART0_BASE + 0x38),
-    UART0_RIS    = (UART0_BASE + 0x3C),
-    UART0_MIS    = (UART0_BASE + 0x40),
-    UART0_ICR    = (UART0_BASE + 0x44),
-    UART0_DMACR  = (UART0_BASE + 0x48),
-    UART0_ITCR   = (UART0_BASE + 0x80),
-    UART0_ITIP   = (UART0_BASE + 0x84),
-    UART0_ITOP   = (UART0_BASE + 0x88),
-    UART0_TDR    = (UART0_BASE + 0x8C),
-};
 
 
 void uart_init()
@@ -90,19 +58,4 @@ void uart_puts(const char* str)
 {
    for ( size_t i = 0; str[i] != '\0'; i ++)
        uart_putc((unsigned char)str[i]);
-}
-
-void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
-{
-   (void) r0;
-   (void) r1;
-   (void) atags;
-
-   uart_init();
-   uart_puts("Hello, kernel World!\r\n");
-
-   while(1) {
-       uart_putc(uart_getc());
-       uart_putc('\n');
-   }
 }
